0d4328c1bc7f216b3c043b53c88ff9a5adc2d3bd
Cristian Torres <cristiantorresfl@gmail.com>
2022-10-27 19:28:09 -0300

    estoy subiendo mi prueba de js

---
+++ Programacion carpeta principal/JavaScript/PruebaJS.js
@@ -0,0 +1,69 @@
+/* Crear un función que tome el 100% del costo los días Lunes, miércoles, viernes, sábado y domingo
+// Los días martes y jueves que tienen descuento aplicar 80% del costo. 
+
+-variable Subtotal sin descuento (Lunes, miercoles, viernes,sabado,Domingo)
+-variable Subtotal días con descuento (Martes, jueves)
+-Variable Total Final; 
+(días sin descuento*1 + Variables días con descuento*0.8)
+
+//El total final corresponde a la suma de los días con descuento + los días sin descuento. 
+
+
+--------------------------------------------------------------------------
+2) //Crear función que aplique 5% de descuento por la compra de 3 productos.
+ //Si al menos, uno de ellos tiene un valor mayor a 10.000
+
+ //Condicional if, si compra 3 productos y alguno es mayor a 10.000
+ // Retorna descuento del 5% a la suma total.
+
+Var producto (1)
+Var Producto (2)
+var producto (3)
+
+Var (Suma producto (1+2+3))
+
+Funcion IF producto (1) > 10.000 
+Funcion IF producto (2) > 10.000 
+Funcion IF producto (3) > 10.000 
+
+Console log; Var Suma producto*0.05 
+//Se cumple condicion de al menos 1 producto es mayor a 10.000 y se aplica descuento 5 %.
+
+else; No se cumple primer paso para descuento.
+
+// Si no cumplen las condiciones no se aplica el 5% de descuento a la suma 
+total.
+
+3)
+
+//Realizar una funcion que se encargue de verificar si hay errores y arreglarlos.
+//mostrandole al console log el mensaje de error si hay numero negativo y el indice donde se encontraba.
+
+//tomar todos los productos desde el primero hasta el último, como muestra el ejemplo desde el primero al último.
+
+var costo producto (a)
+var costo producto (b)
+var costo producto (c)
+var costo producto (d)
+var costo producto (e)
+
+Función Math. sign asignarle a cada producto
+//asi saber en detalle que producto fue el negativo
+
+console log "error, hay un negativo y es este número"
+else: "no se encontró error"
+
+//Se cumple la función y avisa si hay error, mostrando directamente el índice si se aplica a cada función.
+
+
+
+4) //Crear función que muestre el valor mas costoso y menos costoso
+
+Función orden (200,5500,900,7000,500,300)
+Se aplica la función para que ordene de menor a mayor
+El primer número seria el menor y el ultimo el mayor por lo que queda que 
+
+//número menor es 300 y mayor es 7000. Por consiguiente, el menos costoso y el más costoso.
+
+
+*/
